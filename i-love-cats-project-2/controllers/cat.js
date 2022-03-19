b3eade01890c43868d224f3bf8b022535c4da25c
lyee197 <liamsyee@gmail.com>
2022-03-19 16:15:39 -0400

    Organized routes

--- i-love-cats-project-2/controllers/cat.js
+++ i-love-cats-project-2/controllers/cat.js
@@ -38,6 +38,45 @@ router.use((req, res, next) => {
 let catArray
 
 router.get('/', (req, res) => {
+	// find the cats
+	Cat.find({})
+		// then render a template AFTER they're found
+		.then((cats) => {
+			const username = req.session.username
+			const loggedIn = req.session.loggedIn
+			// console.log(fruits)
+			res.render('cat/index', { cats, username, loggedIn })
+		})
+		// show an error if there is one
+        .catch((error) => {
+            res.redirect(`/error?error=${error}`)
+        })
+})
+
+
+
+////////////////////////////////////////////////////////////////////////
+// index that shows only the user's examples
+////////////////////////////////////////////////////////////////////////
+router.get('/favorite', (req, res) => {
+    // find the cats
+    Cat.find({ owner: req.session.userId})
+        // find user
+        .then((cat) => {
+            //log user
+            const username = req.session.username
+            const loggedIn = req.session.loggedIn
+            res.render('cat/index', { cat, username, loggedIn})
+        })
+        // show an error if there is one
+        .catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+////////////////////////////////////
+// 'new' cat
+////////////////////////////////////
+router.get('/new', (req, res) => {
     fetch(`http://api.thecatapi.com/v1/images/search`,  {
 		method: 'GET',
 		headers: {
@@ -52,62 +91,17 @@ router.get('/', (req, res) => {
     .then((catData) => {
         const { username, userId, loggedIn } = req.session
 		console.log(catData[0])
-        res.render('cat/index', { catStuff: catData, username, loggedIn })
+        res.render('cat/new', { catStuff: catData, username, loggedIn })
 	})
-    .catch(error => console.error(error))
-    // Cat.find({ owner: userId })
-    // const { username, userId, loggedIn } = req.session
-    //     .then((cat) => {
-    //         res.render('cat/index', { cat, username, loggedIn })
-    //     })
-    //     // show an error if there is one
-	// 	.catch(error => {
-	// 		res.redirect(`/error?error=${error}`)
-	// 	})
+    .catch((error) => {
+        res.redirect(`/error?error=${error}`)
+    })
 })
-
-// index that shows only the user's examples
-router.get('/favorite', (req, res) => {
-    // find the cats
-    Cat.find({ owner: req.session.userId})
-        // find user
-        .then((cat) => {
-            //log user
-            const username = req.session.username
-            const loggedIn = req.session.loggedIn
-            res.render('cat/index', { cat, username, loggedIn})
-        })
-        // show an error if there is one
-        .catch((error) => {
-            res.json({ error })
-        })
-})
-
-// new route -> GET route that renders our page with the form
-router.get('/:id/edit', (req, res) => {
-    Cat.findById( req.params.id )
-        .then((cat) => {
-            const { username, userId, loggedIn } = req.session
-            res.render('cat/edit', { cat, username, loggedIn})
-        })
-        .catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
 //show
-router.get('/:id', (req, res) => {
-    Cat.findById( req.params.id )
-        .then((cat) => {
-            const { username, userId, loggedIn } = req.session
-            res.render('cat/show', { cat, username, loggedIn})
-        })
-        .catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // create -> POST route that actually calls the db and makes a new document <----
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 router.post('/', (req, res) => {
     //
     imgUrl = req.body.url
@@ -127,7 +121,25 @@ router.post('/', (req, res) => {
 //     // we need to get the 
 router.edit
 // })
+
+////////////////////////////////////////////////////////////////////////
+// new route -> GET route that renders our page with the form
+////////////////////////////////////////////////////////////////////////
+router.get('/:id/edit', (req, res) => {
+    Cat.findById( req.params.id )
+        .then((cat) => {
+            const { username, userId, loggedIn } = req.session
+            res.render('cat/edit', { cat, username, loggedIn})
+        })
+        .catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+
+
+////////////////////////////////////
 // update route
+////////////////////////////////////
 router.put('/:id', (req, res) => {
     const catId = req.params.id
     Cat.findByIdAndUpdate(catId, req.body, { new: true})
@@ -140,11 +152,38 @@ router.put('/:id', (req, res) => {
 		})
 })
 
+////////////////////////////////////
 // show route
+////////////////////////////////////
+router.get('/:id', (req, res) => {
+    Cat.findById( req.params.id )
+        .then((cat) => {
+            const { username, userId, loggedIn } = req.session
+            res.render('cat/show', { cat, username, loggedIn, userId})
+        })
+        .catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
 
+////////////////////////////////////
 // delete route
+////////////////////////////////////
+router.delete('/:id', (req, res) => {
+	// get the fruit id
+	const catId = req.params.id
+	// delete the fruit
+	Cat.findByIdAndRemove(catId)
+		.then((cat) => {
+			console.log('this is the response from FBID', cat)
+			// res.redirect('/fruits')
+		})
+        .catch((error) => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
 
+////////////////////////////////////
 // Export the Router
+////////////////////////////////////
 module.exports = router
-
-//
